76487ecc35ec81124c9cd8355791f339b7fe2c40
David <villamizarandresdavid@gmail.com>
2025-03-25 17:49:36 -0400

    25-03-2025 delete by app

---
+++ app/api/app/timestamp/fiscal/[delet]/route.ts
@@ -0,0 +1,26 @@
+import { connectDB } from "@/libs/db";
+import timestamps from "@/models/timestamps";
+import { NextResponse } from "next/server";
+
+connectDB();
+
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { delet: string } }
+) {
+  const { delet } = await params;
+  console.log("delet:", delet);
+
+  try {
+    //eliminar el registro
+    await timestamps.findByIdAndDelete(delet);
+    return NextResponse.json(
+      { message: "Firma eliminada correctamente" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json((error as Error).message, { status: 400 });
+  }
+}
